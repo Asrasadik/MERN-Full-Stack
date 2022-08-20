60d3c6b47be9465020f6d7cf2d1f6afd7862e8ee
Asrasadik <47492644+Asrasadik@users.noreply.github.com>
2022-08-21 02:44:11 +0530

    Module

---
+++ Javascript Fundamentals/Pratice/Class/models/car.js
@@ -0,0 +1,6 @@
+// we can export variable, function etc
+export class Car {
+  constructor(id) {
+    this.id = id;
+  }
+}
---
+++ Javascript Fundamentals/Pratice/Class/modules.js
@@ -0,0 +1,4 @@
+import { Car } from "./models/car";
+
+let car1 = new Car(123);
+console.log(car1.id);
