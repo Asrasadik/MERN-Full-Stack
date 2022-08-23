004291587e3dda2a1e3b40becfc5df86f52be37b
Asrasadik <47492644+Asrasadik@users.noreply.github.com>
2022-08-23 22:44:41 +0530

    Promises

--- Javascript Fundamentals/Pratice/Promises/script.js
+++ Javascript Fundamentals/Pratice/Promises/script.js
@@ -1,3 +1,32 @@
 console.log('"Promises and Error Handling"');
 
 //-------------------------------------------------------------
+try {
+  let car = newCar;
+} catch (error) {
+  console.log("error:", error);
+} finally {
+  console.log("continuing....");
+}
+//--------------throw(own/developer error)------------------------
+try {
+  throw new Error("My custom error");
+} catch (error) {
+  console.log("error:", error);
+} finally {
+  console.log("finally always executes");
+}
+
+//------------Creating Promise---------------
+let promise = new Promise((resolve, reject) => {
+  setTimeout(resolve, 100, "someValue");
+});
+console.log(promise);
+//-------------settling Promise------------------
+let settlepromise = new Promise((resolve, reject) => {
+  setTimeout(resolve, 100, "someValue");
+});
+settlepromise.then(
+  (value) => console.log("fulfilled: " + value),
+  (error) => console.log("rejected: " + error)
+);
